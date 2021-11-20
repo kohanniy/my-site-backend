0d9de193726c9cc2dba737e1fbf8f8278679f490
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-20 12:45:32 +0500

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '903523fcd57822de7c154a29ee648cd3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a79d54f9-1c40-452d-904d-a1b315138190'
+};
