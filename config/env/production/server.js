60c319791ad0992ebd7f6256643bbba0faf841c6
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-21 17:52:46 +0500

    2

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,6 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
+  admin: {
+    path: "/dashboard",
+  },
 });
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "server",
+      name: "app",
       script: "npm",
       args: "start",
       instances: "max",
       exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development",
-      },
       env_production: {
         NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
       },
     },
   ],
