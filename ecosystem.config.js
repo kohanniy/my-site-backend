d3d21a769311ab9d7578f88ebdabf5b276604908
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-21 18:32:59 +0500

    4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,14 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "npm",
-      args: "start",
+      script: "./server.js",
       instances: "max",
       exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+      },
       env_production: {
         NODE_ENV: "production",
-      },
-      env_development: {
-        NODE_ENV: "development",
       },
     },
   ],
