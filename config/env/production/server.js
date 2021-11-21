9145f3fb057784af74b70d2f11767f416bd3e70e
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-11-21 17:19:00 +0500

    add entry point for pm2

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,3 @@
-const strapi = require("strapi");
-strapi().start();
-
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
 });
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "app",
+      name: "server",
       script: "npm",
       args: "start",
       instances: "max",
